cf827f5fcf7abe081bba1e368194ad57ae0388be
zaraboy <dsqrov@abv.bg>
2016-01-17 00:41:50 +0000

    WORKS

--- try.php
+++ try.php
@@ -10,22 +10,24 @@ $dbname="Users";
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
-//$con->close();
-
-if(!empty($_POST['user'])){
-
-echo "kor";
+if ($con->connect_errno){
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
 }
 
+//$con->close();
 
-$query1 = "Select Username from Users";
-if ($stmt = $con->prepare($query1)) {
-    $stmt->execute();
-    $stmt->bind_result($Username);
-    while ($stmt->fetch()) {
-        printf("%s\n", $Username);
+session_start();
+$query1 = "Select Username,Password from Users where Username = '$_POST[user]' and Password = '$_POST[pass]'";
+if ($result = $con->query($query1)){
+    $row = $result -> fetch_row();
+        //printf("%s,%s\n",$row[0],$row[1]);
     }
-    $stmt->close();
+if(!empty($row[0])&& !empty($row[1])){
+    $_SESSION[0] = $row[1];
+    echo"Success";
+ }else{
+    echo "Wrong username or password !";
 }
 
 
